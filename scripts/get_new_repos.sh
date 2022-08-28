4a43e297005b01a98e897d3a482d10ac8b8c8520
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-08-29 01:18:36 +0900

    Add the script

---
+++ scripts/get_new_repos.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ue
+
+date_range=$(date --date="3 day ago" +"%Y-%m-%d")
+gh api --method=GET search/repositories -f q="nvim created:>$date_range sort:updated" -f per_page=100 >/tmp/repo.json
+cat /tmp/repo.json | jq -r '.items[] |
+  select(.full_name | test("/nvim-") or endswith(".nvim")) |
+  select(.full_name | test("/nvim-config") or test("/nvim-conf") or endswith("/nvim") | not) |
+  select(.language != "Vim Script") |
+  select(contains({description: "colorscheme"}) or contains({description: "config"}) | not) |
+  select(.topics | index("neovim-colorscheme") or index("colorscheme") or index("dotfiles") | not)
+  .full_name' >/tmp/repo2.txt
+# if [[ -e /tmp/repo.3.txt ]]; then
+# 	rm /tmp/repo3.txt
+# fi
+# cat /tmp/repo2.txt | xargs -i bash -c "gh api user/starred/{} >/dev/null 2>&1 || echo {} >> /tmp/repo3.txt"
