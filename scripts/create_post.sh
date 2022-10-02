16fc8cde596915130417f4a4878aef9a2907237d
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-10-03 01:06:51 +0900

    Add the feature to create posts

---
+++ scripts/create_post.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -ue
+
+if [[ ! -e /tmp/repo3.txt ]]; then
+	exit 0
+fi
+
+if [[ $(wc -l </tmp/repo3.txt) -eq 0 ]]; then
+	exit 0
+fi
+
+REPOS=($(cat /tmp/repo3.txt | sed 's#/#--#g'))
+DATE=$(date +"%Y-%m-%d")
+
+for r in "${REPOS[@]}"; do
+	if compgen -G ./_posts/????-??-??-$r.html >/dev/null; then
+		continue
+	fi
+	repo=${r//--/\/}
+	touch ./_posts/$DATE-$r.html
+	tee ./_posts/$DATE-$r.html <<EOF >/dev/null
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url='https://github.com/$repo'" />
+  </head>
+  <body>
+    <a href="https://github.com/$repo">$repo</a>
+  </body>
+</html>
+EOF
+	echo "Add: $repo"
+done
