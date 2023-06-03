ab3b06ea08d586fe12b32005a39cbe5eddfbcd8c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-06-03 22:18:54 +0900

    Apply pagination

--- scripts/get_new_repos.sh
+++ scripts/get_new_repos.sh
@@ -3,24 +3,37 @@ set -ue
 
 date_range_from=$(date --date="3 day ago" +"%Y-%m-%d")
 date_range_to=$(date --date="2 day ago" +"%Y-%m-%d")
-gh api --method=GET search/repositories -f q="nvim in:name size:>1 template:false archived:false created:${date_range_from}..${date_range_to} sort:updated" -f per_page=100 >/tmp/repo.json
+
+if [[ -e /tmp/repo.json  ]]; then
+	rm /tmp/repo.json
+fi
+
+page=1
+while : ; do
+	result=$(gh api --method=GET search/repositories -f q="nvim created:${date_range_from}..${date_range_to} sort:updated" -f per_page=100 -f page=$page | tee -a /tmp/repo.json)
+	page=$((page + 1))
+	items=$(echo $result | jq -r '.items[]')
+	[ -z "$items" ] && break
+done
+
 cat /tmp/repo.json | jq -r '.items[] | select(.full_name | test("/nvim-") or endswith(".nvim")) |
-  select(.full_name | test("/nvim-config") or
-    test("/nvim-conf") or
-    test("/nvim-cfg") or
-    test("/nvim-settings") or
-    test("/nvim-ide") or
-    test("/nvim-basic-ide") or
-    test("/nvim-ide-basic") or
-    test("/nvim-from-scratch") or
-    endswith("/nvim") or
-    endswith("/nvim-setup") or
-    endswith("/nvim-lua") or
-    endswith("/.nvim") or
-    test("theme") or test("dotfiles") | not) |
-  select(.language != "Vim Script") |
-  select(contains({description: "colorscheme"}) or contains({description: "config"}) | not) |
-  select(.topics | index("neovim-colorscheme") or index("neovim-theme") or index("colorscheme") or index("dotfiles") | not)
+		select(.full_name | test("/nvim-config") or
+			test("/nvim-conf") or
+			test("/nvim-ide") or
+			test("/nvim-basic-ide") or
+			test("/nvim-ide-basic") or
+			test("/nvim-from-scratch") or
+			endswith("/nvim") or
+			endswith("/nvim-setup") or
+			endswith("/nvim-lua") or
+			endswith("/.nvim") or
+			test("theme") or
+			test("dotfiles") or
+			contains("dots") or
+			contains("init") | not) |
+		select(.language != "Vim Script") |
+		select(contains({description: "colorscheme"}) or contains({description: "config"}) | not) |
+		select(.topics | index("neovim-colorscheme") or index("neovim-theme") or index("colorscheme") or index("dotfiles") | not)
 .full_name' >/tmp/repo2.txt
 if [[ -e /tmp/repo.3.txt ]]; then
 	rm /tmp/repo3.txt
